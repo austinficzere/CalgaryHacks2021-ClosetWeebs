f5e6c9168eabb68f861432f97c88cca2e13c7f6c
Manbir Sandhu <mana24129@hotmail.com>
2021-02-13 18:50:05 -0700

    up

--- app.py
+++ app.py
@@ -1,18 +1,40 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from person import Person
+from database import Database
 
 app = Flask(__name__)
+data = Database()
 
 @app.route('/')
 def index():
-    pass
+    return redirect(url_for('login'))
 
 @app.route("/login", methods = ['POST','GET'])
 def login():
+    if request.method == 'POST':
+        loginValidate(request.form)
     return render_template('login.html')
+
+def loginValidate(form):
+    pass
+
 
 @app.route("/createAccount", methods = ['POST','GET'])
 def createAccount():
+    if request.method == 'POST':
+        newPerson = createPerson(request.form)
     return render_template('createAccount.html')
+
+
+def addPerson(form):
+    #check if person exists
+    # add to database
+    pass
+
+
+def createPerson():
+    # create the person
+    pass
 
 if __name__ == "__main__":
     app.run(debug=True)
