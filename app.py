7170fcc22146646d96fb61d54283544a51aaf5f7
Manbir Sandhu <mana24129@hotmail.com>
2021-02-13 20:27:04 -0700

    added login/singup

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from database import Database
 
 app = Flask(__name__)
 data = Database()
+mapToNum = {'sport':0,'game':1,'read':2,'code':3,'music':4, 'stock' : 5, 'movies' : 6}
 
 @app.route('/')
 def index():
@@ -16,25 +17,40 @@ def login():
     return render_template('login.html')
 
 def loginValidate(form):
-    pass
+    if data.isUserPassValid(request.form['username'],request.form['pswd']):
+        print("LOGINED")
+    else:
+        print("no")
 
 
 @app.route("/createAccount", methods = ['POST','GET'])
 def createAccount():
     if request.method == 'POST':
         newPerson = createPerson(request.form)
+        addPerson(request.form,newPerson)
     return render_template('createAccount.html')
 
-
-def addPerson(form):
-    #check if person exists
-    # add to database
-    pass
+@app.route("/users/<name>")
+def createProfile():
+    render_template("profileTemplate.html",data.getPersons)
 
 
-def createPerson():
-    # create the person
-    pass
+def addPerson(form,person):
+    user = request.form['username']
+    passw = request.form['pswd']
+    data.addUser(user,passw)
+    data.addPerson(user,person)
+    data.closeData()
+
+
+def createPerson(form):
+    interest = []
+    for key in mapToNum.keys():
+        if key in form:
+            if form[key] == 'on':
+                interest.append(mapToNum[key])
+    return Person(form['name'],int(form['year+']),form['major'],form['concen'],[],interest,form['descrip'],int(form['priority']))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
